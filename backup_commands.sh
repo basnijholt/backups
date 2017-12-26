09376cb0a5d7a5f678b56e96add50ac11aecb174
Bas Nijholt <basnijholt@gmail.com>
2017-12-26 14:35:00 +0100

    small changes to style

--- backup_commands.sh
+++ backup_commands.sh
@@ -3,25 +3,25 @@
 # Get WiFi SSID from https://stackoverflow.com/a/4481019/3447047
 export SSID="`airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`"
 
-# Folder
-export rtb=$HOME/dotfiles/rsync-time-backup
+# rsync-time-backup script
+export rtb=$HOME/dotfiles/rsync-time-backup/rsync-time-backup/rsync_tmbackup.sh
 
 
-# Backup Seafile folder local to 4TB
+# Backup Seafile folder from local to 4TB
 if [[ -d /Volumes/4TB ]]; then 
-  $rtb/rsync_tmbackup.sh $HOME/Seafile /Volumes/4TB/Backup-Seafile $rtb/excluded_patterns.txt
+  $rtb $HOME/Seafile /Volumes/4TB/Backup-Seafile excluded_patterns.txt
 fi
 
 
 ## Backup Seafile folder to 2TB @ RPi
 if [[ $SSID = 'Kwalitijd-Safe' ]]; then
-  $rtb/rsync_tmbackup.sh $HOME/Seafile root@192.168.31.3:/media/usb/Backup-Seafile $rtb/excluded_patterns.txt
+  $rtb $HOME/Seafile root@192.168.31.3:/media/usb/Backup-Seafile excluded_patterns.txt
 fi
 
 
 # 4TB to 2TB @ RPi
 if [[ (-d /Volumes/4TB) && ($SSID == 'Kwalitijd-Safe') ]]; then 
   for folder in Encrypted Film Games iTunes Photos Websites; do
-      $rtb/rsync_tmbackup.sh /Volumes/4TB/$folder root@192.168.31.3:/media/usb/Backup-$folder $rtb/excluded_patterns.txt
+      $rtb /Volumes/4TB/$folder root@192.168.31.3:/media/usb/Backup-$folder excluded_patterns.txt
   done 
 fi
