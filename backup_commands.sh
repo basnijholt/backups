d4d05df519496993bbf0868d75b7650dd2ae1e42
Bas Nijholt <basnijholt@gmail.com>
2018-04-11 12:56:03 +0200

    full path for airport

--- backup_commands.sh
+++ backup_commands.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Get WiFi SSID from https://stackoverflow.com/a/4481019/3447047
-export SSID="`airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`"
+export SSID="`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`"
 export MY_NETWORK='Kwalitijd-Safe'
 
 # rsync-time-backup script
@@ -12,13 +12,13 @@ export rtb=$DIRECTORY/rsync-time-backup/rsync_tmbackup.sh
 
 # Backup Seafile folder from local to 4TB
 if [[ -d /Volumes/4TB ]]; then 
-  $rtb $HOME/Seafile /Volumes/4TB/Backup-Seafile excluded_patterns.txt
+  $rtb $HOME/Sync /Volumes/4TB/Backup-Sync excluded_patterns.txt
 fi
 
 
 ## Backup Seafile folder to 2TB @ RPi
 if [[ $SSID = $MY_NETWORK ]]; then
-  $rtb $HOME/Seafile root@192.168.31.3:/media/usb/Backup-Seafile excluded_patterns.txt
+  $rtb $HOME/Sync root@192.168.31.3:/media/usb/Backup-Sync excluded_patterns.txt
 fi
 
 
