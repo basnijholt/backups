f54105569256aa5643abe023843cca623bcb18d7
Bas Nijholt <basnijholt@gmail.com>
2019-03-05 13:47:35 +0100

    only run when script isn't running

--- backup_commands.sh
+++ backup_commands.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+# Check if the script is already running
+# https://stackoverflow.com/a/45429634/3447047
+if ps ax | grep $0 | grep -v $$ | grep bash | grep -v grep
+then
+    echo "The script is already running."
+    exit 1
+fi
+
 # Get WiFi SSID from https://stackoverflow.com/a/4481019/3447047
 export SSID="`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`"
-export MY_NETWORK='Kwalitijd-Safe'
+export MY_NETWORK='Kwalitijd-Snel-5GHz'
 
 # rsync-time-backup script
 DIRECTORY=$(cd `dirname $0` && pwd)
@@ -24,7 +32,9 @@ fi
 
 # 4TB to 2TB @ RPi
 if [[ (-d /Volumes/4TB) && ($SSID == $MY_NETWORK) ]]; then 
-  for folder in Encrypted Film Games iTunes Photos Websites; do
+  for folder in Encrypted iTunes Photos Websites; do
       $rtb /Volumes/4TB/$folder root@192.168.31.3:/media/usb/Backup-$folder excluded_patterns.txt
   done
 fi
+
+# for folder in Encrypted Film Games iTunes Photos Websites; do
